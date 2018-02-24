3fbd04ea54f4d3429b9557a014a4564d9a6088a8
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-02-24 16:38:08 -0500

    random

--- sequencing/sequencing/settings.py
+++ sequencing/sequencing/settings.py
@@ -100,5 +100,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname("D:/home/site/wwwroot/sequencing/")
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
