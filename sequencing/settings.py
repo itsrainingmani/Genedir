c06c7bb03e79bc97031cec0af9350ce94950fb7e
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-02-24 16:31:06 -0500

    Changed the string literals

--- sequencing/settings.py
+++ sequencing/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname("D:/home/site/wwwroot/")
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 try:
