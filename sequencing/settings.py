43865e7c3862b4ecc0769f44d586150261236978
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-02-24 16:28:18 -0500

    Added Static_root setting for azure

--- sequencing/settings.py
+++ sequencing/settings.py
@@ -105,6 +105,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 try:
     from .local_settings import *
